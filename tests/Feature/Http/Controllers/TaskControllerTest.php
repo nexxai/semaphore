612d2e1fe123bc11dfd2b3f480b168e84c30c45e
JT Smith <nexxai@gmail.com>
2025-07-11 14:59:28 -0600

    Fixing test to work with reverb

--- tests/Feature/Http/Controllers/TaskControllerTest.php
+++ tests/Feature/Http/Controllers/TaskControllerTest.php
@@ -3,18 +3,23 @@
 use App\Mail\FirstTaskAddedToDay;
 use App\Models\Day;
 use App\Models\Task;
+use App\Models\User;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Mail;
 
+use function Pest\Laravel\actingAs;
 use function Pest\Laravel\postJson;
 
 beforeEach(function () {
     Mail::fake();
+    Event::fake();
 
     $this->day = Day::factory()->create();
     $this->task = Task::factory()->create();
 });
 
 it('will send an email if the task is the first one being added to this day', function () {
+    actingAs(User::factory()->create());
     postJson(route('tasks.add'), [
         'taskId' => $this->task->id,
     ])->assertOk();
