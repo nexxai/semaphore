857eb26544309838cfd202ad198d8ce7aad63173
JT Smith <nexxai@gmail.com>
2025-07-12 20:41:38 -0600

    Resizing the text on mobile

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -4,7 +4,7 @@ import { Input } from '@/components/ui/input';
 import { type SharedData, type Task } from '@/types';
 import { Head, router, usePage } from '@inertiajs/react';
 import { useEcho } from '@laravel/echo-react';
-import { PlusIcon } from 'lucide-react';
+import { Plus } from 'lucide-react';
 import { useState } from 'react';
 
 interface Day {
@@ -46,15 +46,13 @@ export default function Welcome({ tasks, current_day }: { tasks?: Task[]; curren
                 <div className="flex w-full items-center justify-center opacity-100 transition-opacity duration-750 lg:grow starting:opacity-0">
                     <main className="flex w-full flex-col-reverse lg:max-w-4xl lg:flex-row">
                         <div className="flex w-full flex-col items-center justify-center gap-6 px-6 py-8 lg:w-4/5 lg:items-start lg:px-12 lg:py-16">
-                            <h1 className="text-3xl leading-tight font-bold tracking-tight text-[#1b1b18] lg:text-4xl dark:text-[#EDEDEC]">
-                                Tasklist
+                            <h1 className="text-5xl leading-tight font-bold tracking-tight text-[#1b1b18] lg:text-4xl dark:text-[#EDEDEC]">
+                                Semaphore
                             </h1>
 
-                            <h2 className="text-xl leading-tight font-semibold tracking-tight text-[#1b1b18] lg:text-2xl dark:text-[#EDEDEC]">
-                                To-do
-                            </h2>
+                            <h2 className="text-3xl leading-tight font-semibold tracking-tight text-[#1b1b18] dark:text-[#EDEDEC]">To-do</h2>
 
-                            <div className="w-full text-base text-[#1b1b18] sm:w-1/2 lg:w-2/3 dark:text-[#EDEDEC]">
+                            <div className="w-full text-lg text-[#1b1b18] sm:w-1/2 lg:w-2/3 dark:text-[#EDEDEC]">
                                 {current_day.tasks && current_day.tasks.length > 0 ? (
                                     <ul className="space-y-1">
                                         {current_day.tasks.map((task) => (
@@ -68,21 +66,21 @@ export default function Welcome({ tasks, current_day }: { tasks?: Task[]; curren
                                 )}
                             </div>
 
-                            <h2 className="text-xl leading-tight font-semibold tracking-tight text-[#1b1b18] lg:text-2xl dark:text-[#EDEDEC]">
+                            <h2 className="text-3xl leading-tight font-semibold tracking-tight text-[#1b1b18] dark:text-[#EDEDEC]">
                                 Available tasks
                             </h2>
-                            <div className="w-full space-y-1 text-base text-[#1b1b18] sm:w-1/2 lg:w-2/3 dark:text-[#EDEDEC]">
+                            <div className="w-full space-y-1 text-lg text-[#1b1b18] sm:w-1/2 lg:w-2/3 dark:text-[#EDEDEC]">
                                 {tasks &&
                                     tasks.length > 0 &&
                                     tasks.map((task) => (
                                         <Badge
                                             variant="secondary"
                                             size="lg"
-                                            className="mr-2 transition hover:scale-105 hover:cursor-pointer"
+                                            className="mr-2 text-lg transition hover:scale-105 hover:cursor-pointer"
                                             key={task.id}
                                             onClick={() => addTask(task.id)}
                                         >
-                                            <PlusIcon />
+                                            <Plus height="32" width="32" />
                                             {task.name}
                                         </Badge>
                                     ))}
@@ -91,7 +89,7 @@ export default function Welcome({ tasks, current_day }: { tasks?: Task[]; curren
                                     <Input
                                         type="text"
                                         placeholder="New task name..."
-                                        className="mt-2 mb-2 w-full max-w-xs"
+                                        className="mt-2 mb-2 w-full max-w-xs text-lg"
                                         onKeyDown={(e) => {
                                             if (e.key === 'Enter') {
                                                 const newTaskName = (e.target as HTMLInputElement).value;
@@ -109,10 +107,10 @@ export default function Welcome({ tasks, current_day }: { tasks?: Task[]; curren
                                     <Badge
                                         variant="secondary"
                                         size="lg"
-                                        className="mr-2 transition hover:scale-105 hover:cursor-pointer"
+                                        className="mr-2 text-lg transition hover:scale-105 hover:cursor-pointer"
                                         onClick={() => toggleShowNewTaskInput()}
                                     >
-                                        <PlusIcon />
+                                        <Plus height="32" width="32" />
                                         Add New Task
                                     </Badge>
                                 )}
